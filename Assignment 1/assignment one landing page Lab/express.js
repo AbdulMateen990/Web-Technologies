a862a18be1499869d65c37289373745ce1e5e021
AbdulMateen <mateensunny10@gmail.com>
2024-03-24 14:19:58 +0500

    form changes

---
+++ Assignment 1/assignment one landing page Lab/express.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// let server = express();
+
+// server.get('/', (req, res) => {
+//   res.send('H');
+// });
+
+// server.listen(4000);
---
+++ Practice/express-app.js
@@ -0,0 +1,13 @@
+//npm i express (run this command in the folder where you have this file)
+//npm i nodemon (run this once in liftime to get rid of manual server)
+
+
+// const express = Request('express');
+// let server = express();
+// // server.use(express.static("public"));
+
+// server.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// server.listen(4000);
